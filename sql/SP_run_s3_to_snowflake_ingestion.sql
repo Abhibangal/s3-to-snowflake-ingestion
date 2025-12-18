5e4676d7ecad1d6741805ce1e0ad82e192558e7a
Abhijit Bangal <abhijit.bangal92@gmail.com>
2025-12-18 16:25:00 +0530

    removed dependency of yaml file and added 1 more table to source env config

--- sql/SP_run_s3_to_snowflake_ingestion.sql
+++ sql/SP_run_s3_to_snowflake_ingestion.sql
@@ -1,27 +1,24 @@
-CREATE OR REPLACE PROCEDURE run_s3_to_snowflake_ingestion(
-    p_config_file STRING,   -- e.g. 'aact_ingest_config.yaml'
-    p_data_source STRING,
-    p_adhoc_id    STRING
+CREATE OR REPLACE PROCEDURE RUN_S3_TO_SNOWFLAKE_INGESTION(
+    P_DATA_SOURCE STRING,
+    P_ADHOC_ID STRING
 )
 RETURNS VARIANT
 LANGUAGE PYTHON
 RUNTIME_VERSION = 3.10
-PACKAGES = ('snowflake-snowpark-python', 'pyyaml')
+PACKAGES = ('snowflake-snowpark-python')
 IMPORTS = (
-  '@CT_PROTOCOL.LINKML.S3_TO_SF_GIT_REPO/branches/dev/ingestion/aws_s3_snf_ingestion.py',
-  '@CT_PROTOCOL.LINKML.S3_TO_SF_GIT_REPO/branches/dev/config/aact_ingest_config.yaml')
-HANDLER = 'main'
+    '@CT_PROTOCOL.LINKML.S3_TO_SF_GIT_REPO/branches/dev/ingestion/aws_s3_snf_ingestion.py'
+)
+HANDLER = 'MAIN'
 EXECUTE AS CALLER
 AS
 $$
-from ingestion.aws_s3_snf_ingestion import run
+from aws_s3_snf_ingestion import run
 
-def main(session, p_config_file, p_data_source, p_adhoc_id):
-    return run(session, p_config_file, p_data_source, p_adhoc_id)
-$$;
-
-
-
-
-
--- call run_s3_to_snowflake_ingestion('aact',null);
+def main(session, p_data_source, p_adhoc_id):
+    return run(
+        session=session,
+        data_source=p_data_source,
+        adhoc_id=p_adhoc_id
+    )
+$$; 
